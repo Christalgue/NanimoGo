a1f92fbc76f3fe9fbf9fd339e9ec2c60051401a5
Pomeg <44205023+Pomeg@users.noreply.github.com>
2019-05-02 09:32:28 +0200

    Inclusion a son propre fichier JS

---
+++ html/inclusion.js
@@ -0,0 +1,32 @@
+// inclusion du header et du footer
+$(document).ready($(function(){
+  $("#header").load("header.html"); 
+  
+  $("#footer").load("footer.html", function() {
+	// dans le processus d'identification, on ajoute des fenêtres modales de confimation
+	// avant de rediriger
+	
+	if (top.location.pathname.endsWith("Question.html") || top.location.pathname.endsWith("Comparaison.html"))
+	{
+	  $( "#footer a" ).click( function (link){
+		$( "<p title='Confirmation'>Voulez-vous vraiment quitter l'identification ?</p>" ).dialog({
+		  resizable: false,
+		  height: "auto",
+		  width: 400,
+		  modal: true,
+		  buttons: {
+			"Oui": function() {
+			  $( this ).dialog( "close" );
+			  document.location = link.currentTarget.href;
+			},
+			"Non": function() {
+			  $( this ).dialog( "close" );
+			}
+		  }
+		});
+    	return false;
+	  });
+	}
+  });
+}));
+
